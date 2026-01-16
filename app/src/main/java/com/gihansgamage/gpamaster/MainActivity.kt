c6ed3e5925dcbc590d5b5146fd7b172514189305
gihansgamage <gsgamage4@gmail.com>
2026-01-16 10:47:09 +0530

    Totally work. analytics not working, select semester when adding grades

---
+++ app/src/main/java/com/gihansgamage/gpamaster/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.gihansgamage.gpamaster
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.gihansgamage.gpamaster.databinding.ActivityMainBinding
+import com.gihansgamage.gpamaster.fragments.AnalyticsFragment
+import com.gihansgamage.gpamaster.fragments.HomeFragment
+import com.gihansgamage.gpamaster.fragments.SemestersFragment
+import com.gihansgamage.gpamaster.fragments.SettingsFragment
+import com.gihansgamage.gpamaster.utils.PrefsHelper
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var prefs: PrefsHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        prefs = PrefsHelper(this)
+
+        // Set default fragment
+        if (savedInstanceState == null) {
+            loadFragment(HomeFragment())
+        }
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_semesters -> {
+                    loadFragment(SemestersFragment())
+                    true
+                }
+                R.id.nav_analytics -> {
+                    loadFragment(AnalyticsFragment())
+                    true
+                }
+                R.id.nav_settings -> {
+                    loadFragment(SettingsFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
